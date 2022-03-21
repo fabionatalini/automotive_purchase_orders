813c581abe6da29ae93a970244a05692788d93be
fabio <fabionatalini@gmail.com>
2022-03-21 12:32:48 +0100

    first commit

---
+++ comandos_github_website.sh
@@ -0,0 +1,16 @@
+
+cd /home/fabio/Documents/automotive_purchase_orders/
+
+git init
+
+git remote add origin https://github.com/fabionatalini/automotive_purchase_orders.git
+
+git branch -M main
+
+git add *
+
+git commit -m "first commit"
+
+git push -u origin main
+
+# Fire up a browser and go to http://fabionatalini.github.io/automotive_purchase_orders
