139882b036e29ad0f3d49890ad1668b9d2111b34
fabio <fabionatalini@gmail.com>
2022-03-21 12:34:49 +0100

    corregido

--- comandos_github_website.sh
+++ comandos_github_website.sh
@@ -5,12 +5,10 @@ git init
 
 git remote add origin https://github.com/fabionatalini/automotive_purchase_orders.git
 
-git branch -M main
-
 git add *
 
-git commit -m "first commit"
+git commit -m "new commit"
 
-git push -u origin main
+git push -u origin master
 
 # Fire up a browser and go to http://fabionatalini.github.io/automotive_purchase_orders
